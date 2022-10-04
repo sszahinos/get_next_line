6a32a10280b6f59f92038fa15bb97da1ce8a404e
Sergio Sanchez Zahinos <sersanch@student.42barcelona.com>
2022-10-04 10:23:22 +0200

    implemented some functions to manage str

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,56 @@
 /*   By: sersanch <sersanch@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 16:17:26 by sersanch          #+#    #+#             */
-/*   Updated: 2022/10/03 16:34:53 by sersanch         ###   ########.fr       */
+/*   Updated: 2022/10/04 10:22:58 by sersanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-char	*ft_find_line(char * line)
+/*
+ * ELIMINAR LINEAS SI ALGUN MALLOC FALLA (REVISAR CADA IF() RETURN NULL)
+ * */
+char	*ft_cut_line(char * line, int index)
 {
+	char	*new_str;
 	int	i;
-	int	readen;
-	char	*str;
 
-	readen = -1;
-	
+	new_str = malloc(sizeof(char) * (ft_strlen(line) - index + 1));//caracter en index se incluye en new_str
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (line[index + i])
+	{
+		new_str[i] = line[index + i];
+		i++;
+	}
+	new_str[i] = '\0';
+	free(line);
+	return (new_str);
+}
+
+char	*ft_join_str(char	*first_line, char *last_line)
+{
+	int		f_l_len[2];
+	int		i_j[2];
+	char	*new_line;
+
+	f_l_len[0] = ft_strlen(first_line);
+	f_l_len[1] = ft_strlen(last_line);
+	new_line = malloc(sizeof(char) * (f_l_len + f_l_len + 1));
+	if (!new_line)
+		return (NULL);
+	i_j[0] = -1;
+	while (++i_j[0] < f_l_len[0])
+		new_line[i_j[0]] = first_line[i_j[0]];
+	i_j[1] = 0;
+	while (i_j[0] < (f_l_len[0] + f_l_len[1]))
+	{
+		new_line[i_j[0]] = last_line[i_j[1]];
+		i_j[0]++;
+		i_j[1]++;
+	}
+	new_line[i_j[0]] = '\0';
+	free(first_line);
+	free(last_line);
+	return (new_line);
 }
 
 char	*get_next_line(int fd)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,18 +1,43 @@
 #include "get_next_line.h"
 
-int	ft_find_nl(t_line_info tli)
+int	ft_strlen(char *str)
 {
-	if (!tli->line)
+	int	i;
+
+	i = 0;
+	while (!str[i])
+		i++;
+	return (i);
+}
+
+int	ft_find_nl(char *str)
+{
+	if (!str)
 		return (-2);
-	//i = 0;
-	while (tli->line[index])
+	i = 0;
+	while (str[i])
 	{
-		if (tli->line[index] == '\n')
-		{
-			tli.index++;
-			return (tli->index[fd]);
-		}
-		tli.index++;
+		if (str[i] == '\n')
+			return (i);
+		i++;
 	}
 	return (-1);
+}
+
+char	*ft_substr(char *str, int len)
+{
+	int	i;
+	char	*sbstr;
+	
+	sbstr = malloc(sizeof(char) * (len + 1));
+	if (!sbstr)
+		return (NULL);
+	i = 0;
+	while (i <= end)
+	{
+		sbstr[i] = str[i];
+		i++;
+	}
+	sbstr[i] = '\0';
+	return (sbstr);
 }
